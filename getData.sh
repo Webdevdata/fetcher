007ce4cdfda281e6b4447cfce0e77df86b82cf6a
Matthew Tylee Atkinson <matatk@gmail.com>
2014-12-08 12:38:35 +0000

    Only grab the top 100,000 sites

--- getData.sh
+++ getData.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
 URLS=/tmp/urls.txt
-if [ ! -f $URLS ]
-then
+URLS_TMP=/tmp/urls.txt.tmp
+if [ ! -f $URLS ]; then
     curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip > top-1m.csv.zip
-    unzip -p top-1m.csv.zip | awk -F',' '{print $2}' > $URLS
+    unzip -p top-1m.csv.zip | awk -F',' '{print $2}' > $URLS_TMP
     rm top-1m.csv.zip
+    head -n 100000 $URLS_TMP > $URLS
+    rm $URLS_TMP
 fi
 DIR=`./downloadr.py create`
 cat $URLS | xargs -I{} -n 1 -P64 ./downloadr.py download {} $DIR
